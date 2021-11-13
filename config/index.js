46ae5741804f30b9fd93148e0494a7a5116e36b3
Alntjan <oliveiracatarino88@gmail.com>
2021-11-13 14:09:24 +0000

    middleware test

--- config/index.js
+++ config/index.js
@@ -56,11 +56,12 @@ module.exports = (app) => {
   );
 
   app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', process.env.ORIGIN); // update to match the domain you will make the request from
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
-    );
+    res.set('Access-Control-Allow-Origin', '*');
+    // res.header('Access-Control-Allow-Origin', process.env.ORIGIN); // update to match the domain you will make the request from
+    // res.header(
+    //   'Access-Control-Allow-Headers',
+    //   'Origin, X-Requested-With, Content-Type, Accept'
+    // );
     next();
   });
 
